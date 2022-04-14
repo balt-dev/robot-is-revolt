c6e41deda1ad9066da90487d2ec7d40a8af010cf
Heptor44 <heptor42@gmail.com>
2022-04-14 15:44:26 -0500

    Error handling listeners no exist in revolt

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -8,9 +8,7 @@ class CommandsCog(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command()
-	async def ping(self, ctx: commands.Context):
-		await ctx.send("h")
+
 
 def setup(bot):
 	bot.add_cog(CommandsCog(bot))
